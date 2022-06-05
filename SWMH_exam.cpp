32a6b5740108d517cf214779d7b937fac1830c09
ryuchan00 <leokun0210@gmail.com>
2022-06-05 12:32:45 +0900

    search_limit

--- SWMH_exam.cpp
+++ SWMH_exam.cpp
@@ -27,7 +27,7 @@ int main(int argc, char *argv[])
   vector<int> database;                 //データベース
   char *dbname = argv[1];               // database.txt
   database = readdatabase_line(dbname); //データベース作成
-  cout << database.size() << endl;
+  std::cout << database.size() << endl;
 
   /*パラメータ*/
   int w = atoi(argv[2]); //ウインドウサイズ
@@ -42,6 +42,8 @@ int main(int argc, char *argv[])
   int vmw = minhash[0].size();      //要素種類数 × 多重度の数
   int multi = atoi(argv[4]);        // wの中の要素の数の上限
   int vm = vmw / multi;             //要素の種類数
+
+  int search_limit = atoi(argv[5]); //delete_val探索時の探索回数
 
   ////////////////////////////////////////////////////////////////
   /*Min-hashに用いるランダムの値のテーブル*/
@@ -72,7 +74,7 @@ int main(int argc, char *argv[])
   /*残っている要素のリストとスライディングウインドウに含まれている数のヒストグラムの作成*/
 
   vector<vector<contents>> Minlist(num_of_hash); //残っている要素のリスト[ハッシュ関数][残ってる要素]
-  vector<deque<int>> histgram(vm);               //要素数0,初期値0
+  vector<deque<int>> histgram(vm);                 //要素数0,初期値0
 
   vector<int> reset_count(num_of_hash, 0);
 
@@ -94,13 +96,10 @@ int main(int argc, char *argv[])
 
   while (t < dmax)
   {
-
-    // double Min_ave=0;
     //出ていく処理
     if (t >= w)
     {
       int out = database[t - w];
-      ;
       histgram[out].erase(histgram[out].begin());
       double sum_length = 0;
       for (int l = 0; l < num_of_hash; l++)
@@ -112,8 +111,8 @@ int main(int argc, char *argv[])
           out_count++;
         }
         if (out == min_elem[l].label)
-        { //最小値と同じラベルがストリームデータから出ていく時
-
+        {
+          //最小値と同じラベルがストリームデータから出ていく時
           int min = 5000000;
           int m_label;
           for (int m = 0; m < Minlist[l].size(); m++)
@@ -121,13 +120,14 @@ int main(int argc, char *argv[])
             int Minlist_value = Minlist[l][m].value;
 
             if (min > Minlist_value)
-            { //最小値を調べる
-
+            {
+              //最小値を調べる
               int label = Minlist[l][m].label;
               int value_check = fx[l][label][histgram[label].size()];
 
               if (Minlist_value == value_check)
-              { //割り当て値に間違いがない場合
+              {
+                //割り当て値に間違いがない場合
                 min = Minlist_value;
                 m_label = label;
               }
@@ -135,7 +135,8 @@ int main(int argc, char *argv[])
               {
                 Minlist[l][m].value = value_check;
                 if (min > value_check)
-                { //割り当て値が間違っている場合
+                {
+                  //割り当て値が間違っている場合
                   min = value_check;
                   m_label = label;
                 }
@@ -148,7 +149,7 @@ int main(int argc, char *argv[])
       }
       time_ave_length = sum_length / (double)num_of_hash;
       sum_time_ave_length += time_ave_length;
-    } /*if (t >= 100){*/
+    }
     //入っていく処理////////////////
     In = database[t];
     histgram[In].push_back(t); //とりあえず先に入れておく方針
@@ -166,16 +167,19 @@ int main(int argc, char *argv[])
       int hist_time = histgram[In][pointer]; // = t
 
       while (m >= 0)
-      { // Minlistのスキャン
+      {
+        // Minlistのスキャン
         if (Minlist[l][m].label == In)
-        { //同じラベルのを消す
+        {
+          //同じラベルのを消す
           Minlist[l].erase(Minlist[l].begin() + m);
           same_count++;
         }
         else
         {
-          while (Minlist[l][m].time < hist_time)
-          { //時刻によって判断
+          while (Minlist[l][m].time < search_limit)
+          {
+            //時刻によって判断
             back_IN_num++;
             delete_val = fx[l][In][back_IN_num];
 
@@ -185,12 +189,14 @@ int main(int argc, char *argv[])
               hist_time = histgram[In][pointer];
             }
             else
-            { //ヒストグラムの先頭まで行った
+            {
+              //ヒストグラムの先頭まで行った
               hist_time = -1;
             }
           }
           if (Minlist[l][m].value >= delete_val)
-          { //値を比べる
+          {
+            //値を比べる
             Minlist[l].erase(Minlist[l].begin() + m);
             another_count++;
           }
@@ -204,14 +210,13 @@ int main(int argc, char *argv[])
       Minlist[l].push_back(a);
 
       if (min_elem[l].value > in_value)
-      { //最小値の更新
+      {
+        //最小値の更新
         min_elem[l].value = in_value;
         min_elem[l].label = In;
       }
     }
-    // Min_size = Min_size + Min_ave /r;
 #ifdef DEBUG
-    //   cout << min_elem[0].value << endl;
     if (t >= w)
     {
       vector<int> base_hash(num_of_hash);
@@ -228,7 +233,7 @@ int main(int argc, char *argv[])
           }
         }
         if (min_elem[l].value != min)
-          cout << l << " " << min_elem[l].value << " " << min << endl;
+          std::cout << l << " " << min_elem[l].value << " " << min << endl;
       }
     }
 #endif
@@ -237,13 +242,10 @@ int main(int argc, char *argv[])
   }
 
   ave_length = sum_time_ave_length / t;
-  cout << ave_length << "\n";
+  std::cout << ave_length << "\n";
 
-  cout << "same= " << same_count << " anohter= " << another_count << " out= " << out_count << "\n";
-  // cout<<"\n";
+  std::cout << "same= " << same_count << " anohter= " << another_count << " out= " << out_count << "\n";
   clock_t end = clock(); //ここまで時間測定
-  cout << (double)(end - start) / CLOCKS_PER_SEC << endl;
+  std::cout << (double)(end - start) / CLOCKS_PER_SEC << endl;
   return 0;
 }
-
-// int baseline[num_hash];
