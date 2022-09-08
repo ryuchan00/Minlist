003674f1664298509c2d12bd8ac8a7e9cb138e28
ryuchun00 <leokun0210@gmail.com>
2022-09-08 09:10:39 +0900

    update

--- minhash_test.cpp
+++ minhash_test.cpp
@@ -8,17 +8,21 @@
 #include <cppunit/extensions/HelperMacros.h>
 #include <cppunit/extensions/TestFactoryRegistry.h>
 
+#include <algorithm>
+
 class FunctionTest : public CPPUNIT_NS::TestFixture {
   //テストクラス
   CPPUNIT_TEST_SUITE(FunctionTest);             //登録のスタート
   CPPUNIT_TEST(test_multi_set_strict_jaccard);  //メンバ関数の登録
   CPPUNIT_TEST(test_mh);
+  CPPUNIT_TEST(test_set_intersection);
   CPPUNIT_TEST_SUITE_END();  //登録の終了
 
  protected:
   //   Function *func;
   void test_multi_set_strict_jaccard();
   void test_mh();
+  void test_set_intersection();
 
  public:
   void setUp();
@@ -46,12 +50,32 @@ void FunctionTest::test_multi_set_strict_jaccard() {
 }
 
 void FunctionTest::test_mh() {
-	// 正常に取れているか確認する。
+  // 正常に取れているか確認する。
   std::vector<int> in{0, 0, 1};
   std::vector<int> fx_a{11, 22};
   std::vector<int> fx_b{99, 10, 9};  // 99は使用しない
 
   CPPUNIT_ASSERT_EQUAL(mh(in, fx_a, fx_b), 29);
+
+  // 要素が3のとき
+  in = {0, 1, 2};
+  fx_a = {11, 22, 33, 44};
+  fx_b = {99, 4, 3, 2, 1};
+
+  CPPUNIT_ASSERT_EQUAL(mh(in, fx_a, fx_b), 27);
+}
+
+void FunctionTest::test_set_intersection() {
+  // 多重集合でも機能することを確認する
+  std::vector<int> set1{1, 2};
+  std::vector<int> set2{1, 2, 2};
+  std::vector<int> results{1, 2};
+  std::vector<int> intersection_in;
+
+  std::set_intersection(set1.begin(), set1.end(), set2.begin(), set2.end(), back_inserter(intersection_in));
+  bool equal = intersection_in.size() == results.size() && std::equal(intersection_in.cbegin(), intersection_in.cend(), results.cbegin());
+
+  CPPUNIT_ASSERT_EQUAL(equal, true);
 }
 
 int main(int argc, char const *argv[]) {
