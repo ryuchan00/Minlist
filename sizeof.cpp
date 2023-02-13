30aeee0a12193cd12a1b1310c21e883c61fa6abc
ryuchun00 <leokun0210@gmail.com>
2023-02-13 09:44:08 +0900

    update

--- sizeof.cpp
+++ sizeof.cpp
@@ -8,10 +8,17 @@ int main(int argc, char *argv[]) {
   printf("size of (vector<int>)           : %ld\n", sizeof(std::vector<int>));
   printf("size of (vector<int>(1000))           : %ld\n", sizeof(std::vector<int>(1000)));
   printf("size of (vector<int> *))           : %ld\n", sizeof(std::vector<int> *));
-  printf("size of (vector<deque<int>>)    : %ld\n", sizeof(std::vector<std::deque<int> >));
+  printf("size of (vector<deque<int>>)    : %ld\n", sizeof(std::vector<std::deque<int>>));
   printf("size of (deque<int>)    : %ld\n", sizeof(std::deque<int>));
   printf("size of int           : %ld\n", sizeof(int));
   printf("size of (int *)       : %ld\n", sizeof(int *));
   printf("size of (index)       : %ld\n", sizeof(index));
+  int **a;
+  a = new int *[28];
+  printf("size of (new int)       : %ld\n", sizeof(a));
+  std::vector<std::vector<int>> allocation_pointer(10, std::vector<int>(1000, 0));
+
+  printf("size of (new allocation_pointer)       : %ld\n", sizeof(allocation_pointer));
+
   return 0;
 }
